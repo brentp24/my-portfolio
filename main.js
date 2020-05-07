7a89df012a89a170c5305fdc5d72900dbcaa5d17
Brent Peterson <brentp24@gmail.com>
2020-05-07 09:47:31 -0600

    added background

---
+++ main.js
@@ -0,0 +1,27 @@
+function setBackground() {
+    var client_id = "RCxWMwEhzgzUqQu7IsYsENaOYusewqJSRQ2WcPni-Es";
+    var query = "landscape";
+    var unsplash = "https://api.unsplash.com/photos/random?&query=" + query + "&client_id=" + client_id;
+    //console.log(unsplash);
+    var unsplashWebsite = "https://unsplash.com/";
+    $.ajax({
+        url: unsplash,
+        method: "GET"
+    }).then(function (response) {
+        var imageUrl = response.urls["full"];
+        $("#backgroundPhotographer").html("Photo by <a href=" + response.links.html + ">" + response.user.name + "</a>" + " on <a href=" + unsplashWebsite + ">Unsplash</a>");
+        //console.log(response);
+        $("body")
+            .css("background-image", "url(" + imageUrl + ")")
+            .css("background-position", "center")
+            .css("background-size", "cover");
+    });
+}
+//set Background
+setBackground();
+
+function sayHi() {
+    console.log("hi")
+}
+
+sayHi();
